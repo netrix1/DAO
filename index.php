a53840b71bdfdd2e203c480294eb74c7d4e31f4b
everson <everson@peoplenect.com>
2021-12-31 17:53:44 -0300

    correções em geral

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 require_once("inc".DIRECTORY_SEPARATOR."config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+
+$usuario = new Usuario();
+$usuario->loadById(4);
+echo $usuario;
 ?>
